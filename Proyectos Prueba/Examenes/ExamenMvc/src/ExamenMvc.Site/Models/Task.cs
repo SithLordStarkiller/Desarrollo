e7395640ef84be44fd845717903542281dd6ac73
SithLordStarkiller <SithLordStarkiller@PC-STARKILLER>
2018-02-09 01:01:52 -0600

    Examen Mvc Net Core 1.0

---
+++ Proyectos Prueba/Examenes/ExamenMvc/src/ExamenMvc.Site/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace ExamenMvc.Site.Models
+{
+    using System;
+
+    namespace ExamenMVC.Models
+    {
+        public partial class Task
+        {
+            public Decimal TaskId { get; set; }
+            public Decimal EmployeeId { get; set; }
+            public string TaskName { get; set; }
+        }
+    }
+
+}
