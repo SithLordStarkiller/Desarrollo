c9aebdb621d5c5cc577d9aac453f76ca115f0c48
SithLordStarkiller <SithLordStarkiller@PC-STARKILLER>
2019-01-20 16:29:08 -0600

    Subir archivos a azure

---
+++ Proyectos Prueba/Azure/BlobsUploadFiles/BlobsUploadFiles/BlobsUploadFiles.Site/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlobsUploadFiles.Site.Models;
+
+namespace BlobsUploadFiles.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult UserRegister()
+        {
+            return View();
+        }
+
+        public IActionResult CreateUserRegister(UserViewModel model)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Proyectos Prueba/Azure/BlobsUploadFiles/BlobsUploadFiles/BlobsUploadFiles.Site/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlobsUploadFiles.Site.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+    public class UserViewModel
+    {
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Display(Name = "Fotografia")]
+        [DataType(DataType.Upload)]
+        public IFormFile Photo { get; set; }
+    }
+}
